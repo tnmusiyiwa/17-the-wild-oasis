d1973cbfd4c50ab8b30b822d0e6712cdef9a41c9
Tafadzwa Musiyiwa <tnmusiyiwa@gmail.com>
2024-08-27 19:17:34 +0200

    show checkin details

--- src/features/check-in-out/CheckinBooking.jsx
+++ src/features/check-in-out/CheckinBooking.jsx
@@ -8,6 +8,8 @@ import Button from "../../ui/Button";
 import ButtonText from "../../ui/ButtonText";
 
 import { useMoveBack } from "../../hooks/useMoveBack";
+import { useBooking } from "../bookings/useBooking";
+import Spinner from "../../ui/Spinner";
 
 const Box = styled.div`
   /* Box */
@@ -19,8 +21,9 @@ const Box = styled.div`
 
 function CheckinBooking() {
   const moveBack = useMoveBack();
+  const {booking, isLoading} = useBooking();
 
-  const booking = {};
+  if (isLoading) return <Spinner />
 
   const {
     id: bookingId,
