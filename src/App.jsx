dc3c2273a73435c8693a60f7271e606e6a6ca4ea
Tafadzwa Musiyiwa <tnmusiyiwa@gmail.com>
2024-08-28 00:56:42 +0200

    Fix the Checkin path

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 import {Toaster} from "react-hot-toast";
 import Booking from "./pages/Booking";
-import Checkin from "./pages/CheckIn";
+import Checkin from "./pages/Checkin";
 
 const queryClient = new QueryClient({
   defaultOptions: {
